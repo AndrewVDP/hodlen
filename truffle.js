9b28e8d5d342126a407d9564f8e838462ed94e58
Andrew Van De Poel <andrew.vandepoel@everyonecounts.com>
2017-11-08 20:49:28 -0800

    add favicon image

--- truffle.js
+++ truffle.js
@@ -2,29 +2,11 @@
 require('babel-register')
 
 module.exports = {
-  // networks: {
-  //   development: {
-  //     host: 'localhost',
-  //     port: 8545,
-  //     network_id: '*' // Match any network id
-  //   },
-  //   ropsten: {
-  //     host: 'localhost',
-  //     port: 8545,
-  //     network_id: '3'
-  //   }
-  // }
   networks: {
-	  ropsten: {
-	    provider: function() {
-	      return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/");
-	    },
-	    network_id: '3',
-	  },
-	  development: {
+    development: {
       host: 'localhost',
       port: 8545,
-      network_id: '*'
+      network_id: '*' // Match any network id
     }
-	}
+  }
 }
